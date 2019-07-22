8c5c97ecbb1accfe309c2b4d70335c8d902ebf82
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-07-22 20:38:33 -0300

    Connectando no MongoDB

--- bin/configuration/variables.js
+++ bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb+srv://<username>:<password>@cluster0-h24c5.mongodb.net/test?retryWrites=true&w=majority'
     }
 }
 
--- bin/express.js
+++ bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 // Routers
 const categoriaRouter = require('../routes/categoria-router');
@@ -11,6 +13,9 @@ const app = express();
 // Configuração de parse do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Configurando a conexão com o banco de dados
+mongoose.connect(variables.Database.connection);
 
 //Configurando as rotas
 app.use('/api/categoria', categoriaRouter);
