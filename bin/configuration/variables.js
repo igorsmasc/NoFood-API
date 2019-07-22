1b6e91ae334c4f083296c6fe8dea2a78b3d797a0
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-07-22 20:48:50 -0300

    variables_example

--- bin/configuration/variables.js
+++ bin/configuration/variables.js
@@ -3,7 +3,7 @@ const variables = {
         port: process.env.port || 3000
     },
     Database: {
-        connection: process.env.connection || 'mongodb+srv://<username>:<password>@cluster0-h24c5.mongodb.net/test?retryWrites=true&w=majority'
+        connection: process.env.connection || ''
     }
 }
 
