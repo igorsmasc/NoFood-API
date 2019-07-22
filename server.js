fdafb21d6fea25476ca420c82d48e4dce2937409
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-07-22 20:50:56 -0300

    Corrigindo problemas com o git

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const app = require('../NoFood-API/bin/express');
-const variables = require('../NoFood-API/bin/configuration/variables');
-
-app.listen(variables.Api.port, ()=>{
-    console.info(`API inicializada com sucesso na porta ${variables.Api.port}`);
-})
