2cfefad210659a4abc081db7634085df99af0252
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2019-07-22 22:58:25 -0300

    Adicionando model + crud pt2

---
+++ bin/express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
+
+// Routers
+const categoriaRouter = require('../routes/catergoria-router');
+const produtoRouter = require('../routes/produto-router');
+
+// Criando/Invocando API
+const app = express();
+
+// Configuração de parse do JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Configurando a conexão com o banco de dados
+mongoose.connect(variables.Database.connection);
+
+//Configurando as rotas
+app.use('/api/categoria', categoriaRouter);
+app.use('/api/produto', produtoRouter);
+
+// Exportando API
+module.exports = app;
