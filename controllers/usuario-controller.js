3e3959e6066ca3ca3f30d48afba08221687f7510
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-07-24 11:39:55 -0300

    Criando autenticação da API

--- controllers/usuario-controller.js
+++ controllers/usuario-controller.js
@@ -1,35 +1,64 @@
 'use strict'
 
 const repository = require('../repositories/usuario-repository');
+const validation = require('../bin/helpers/validation');
+const ctrlBase = require('../bin/base/controller-base');
 const _repo = new repository();
+const md5 = require('md5');
 
 function usuarioController() {
-
+    
 }
 
 usuarioController.prototype.post = async (req, res) => { 
-    let result = await _repo.create(req.body);
-    res.status(201).send(result);
+    let _validationContract = new validation();
+
+    _validationContract.isRequired(req.body.nome, 'Informe seu nome');
+    _validationContract.isRequired(req.body.email, 'Informe seu email');
+    _validationContract.isEmail(req.body.email, 'O email informado é inválido');
+    _validationContract.isRequired(req.body.senha, 'A senha informada é inválida');
+    _validationContract.isRequired(req.body.senhaConfirmacao, 'A senha de confirmação é obrigatória');
+    _validationContract.isTrue(req.body.senha != req.body.senhaConfirmacao, 'A senha e a confirmação não são iguais');
+
+    let usuario = await _repo.emailExiste(req.body.email);
+    if(usuario) {
+        _validationContract.isTrue((usuario.nome != undefined), `Já existe o email ${req.body.email} cadastrado em nossa base`);
+    }
+
+    // Criptografa a senha do usuário
+    req.body.senha = md5(req.body.senha);
+
+    ctrlBase.post(_repo, _validationContract, req, res);
 };
 
 usuarioController.prototype.put = async (req, res) => { 
-    let result = await _repo.update(req.params.id, req.body);
-    res.status(202).send(result);
+    let _validationContract = new validation();
+
+    _validationContract.isRequired(req.body.nome, 'Informe seu nome');
+    _validationContract.isRequired(req.body.email, 'Informe seu email');
+    _validationContract.isEmail(req.body.email, 'O email informado é inválido');
+    _validationContract.isRequired(req.params.id, "Informe o id do usuario que será editado")
+   
+    let usuario = await _repo.emailExiste(req.body.email);
+    if(usuario) {
+        _validationContract.isTrue(
+            (usuario.nome != undefined) && 
+            (usuario._id != req.params.id), `Já existe o email ${req.body.email} cadastrado em nossa base`);
+    }
+
+    ctrlBase.put(_repo, _validationContract, req, res);
 };
 
 usuarioController.prototype.get = async (req, res) => {
-    let list = await _repo.getAll();
-    res.status(200).send(list);
+    ctrlBase.get(_repo, req, res);
 };
 
 usuarioController.prototype.getById = async (req, res) => {
-    let usuario = await _repo.getById(req.params.id);
-    res.status(200).send(usuario);
+    ctrlBase.getById(_repo, req, res);
 };
 
 usuarioController.prototype.delete = async (req, res) => { 
-    let deletado = await _repo.delete(req.params.id);
-    res.status(204).send(deletado);
+    ctrlBase.delete(_repo, req, res);
 };
 
 module.exports = usuarioController;
